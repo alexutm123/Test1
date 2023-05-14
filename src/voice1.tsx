e641b74e8a6e2b7cd19169d766277bac803cf12d
alexutm123 <98460526+alexutm123@users.noreply.github.com>
2023-05-14 23:41:46 +0300

    Test2
    voice2

--- src/voice1.tsx
+++ src/voice1.tsx
@@ -1,23 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 function App11() {
-  const [stream, setStream] = useState(null);
+  const [stream, setStream] = useState<MediaStream | null>(null);
 
-  useEffect(() => {
-    const getMediaStream = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-        setStream(stream);
-      } catch (error) {
-        console.error('Error accessing microphone:', error);
-      }
-    };
-
-    getMediaStream();
-  }, []);
+  const handleMicAccess = async () => {
+    try {
+      const mediaStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      setStream(prevState => mediaStream);
+    } catch (error) {
+      console.error('Error accessing microphone:', error);
+    }
+  };
 
   return (
     <div>
+      <button onClick={handleMicAccess}>Allow Microphone Access</button>
       {stream ? 'Microphone is accessible' : 'Please allow microphone permission for this app to work properly.'}
     </div>
   );
